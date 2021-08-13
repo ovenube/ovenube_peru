44d5b77409942c1772918a5e9c68564dc09ed469
seethersan <carlos_jcez@hotmail.com>
2021-08-12 19:27:03 -0500

    [fix] get party and auyhentication error fixed

--- apps/ovenube_peru/ovenube_peru/nubefact_integration/doctype/api_consultas/api_consultas.py
+++ apps/ovenube_peru/ovenube_peru/nubefact_integration/doctype/api_consultas/api_consultas.py
@@ -85,96 +85,99 @@ def get_exchange_rate():
 @frappe.whitelist()
 def get_party(company, tax_id, party_type):
 	try:
-		party = frappe.get_all(party_type, filters={'tax_id': tax_id}, fields=['ruta','token'])
+		party = frappe.get_all(party_type, filters={'tax_id': tax_id})
 
 		if party == []:
-			authentication = frappe.get_doc('API Consultas', company)
-			url = authentication.get('ruta')
-			token = authentication.get('token')
+			authentication = frappe.get_all('API Consultas', fields=['ruta','token'])
+			if authentication:
+				url = authentication[0].get('ruta')
+				token = authentication[0].get('token')
 
-			headers = {
-					'Content-Type':  'application/json'
-			}
-
-			if len(tax_id) == 11:
-				content = {
-					'token': token,
-					'ruc': tax_id
+				headers = {
+						'Content-Type':  'application/json'
 				}
-			elif len(tax_id) == 8:
-				content = {
-					'token': token,
-					'dni': tax_id
-				}
-			response = requests.post(url, headers=headers, data=json.dumps(content))
 
-			party_info = json.loads(response.content)
-			if party_info['success']:
 				if len(tax_id) == 11:
-					party = frappe.get_doc({
-						'doctype': party_type,
-						'party_name': party_info['nombre_o_razon_social'],
-						party_type.lower() + '_name': party_info['nombre_o_razon_social'],
-						'tax_id': party_info['ruc'],
-						'tipo_documento_identidad' if party_type == 'Customer' else 'nombre_tipo_documento': 'REGISTRO ÚNICO DE CONTRIBUYENTES',
-						'codigo_tipo_documento': '6'
-					})
-					party.save()
-
-					address_shipping = frappe.get_doc({
-						'doctype': 'Address',
-						'address_title': party_info['nombre_o_razon_social'],
-						'address_type': 'Shipping',
-						'address_line1': party_info['direccion_completa'],
-						'distrito': party_info['distrito'],
-						'provincia': party_info['provincia'],
-						'departamento': party_info['departamento'],
-						'country': 'Peru'
-					})
-					address_shipping.save()
-
-					address_billing = frappe.get_doc({
-						'doctype': 'Address',
-						'address_title': party_info['nombre_o_razon_social'],
-						'address_type': 'Billing',
-						'address_line1': party_info['direccion_completa'],
-						'distrito': party_info['distrito'],
-						'provincia': party_info['provincia'],
-						'departamento': party_info['departamento'],
-						'country': 'Peru'
-					})
-					address_billing.save()
-
-					shipping_dynamic_link = frappe.get_doc({
-                                'doctype': 'Dynamic Link',
-                                'link_doctype': party_type,
-                                'link_name': party.name,
-                                'parenttype': 'Address',
-                                'parent': address_shipping.name
-                            })
-					shipping_dynamic_link.save()
-
-					billing_dynamic_link = frappe.get_doc({
-                                'doctype': 'Dynamic Link',
-                                'link_doctype': party_type,
-                                'link_name': party.name,
-                                'parenttype': 'Address',
-                                'parent': address_billing.name
-                            })
-					billing_dynamic_link.save()
-										
+					content = {
+						'token': token,
+						'ruc': tax_id
+					}
 				elif len(tax_id) == 8:
-					party = frappe.get_doc({
-						'doctype': party_type,
-						'party_name': party_info['nombre_completo'],
-						party_type.lower() + '_name': party_info['nombre_completo'],
-						'tax_id': party_info['dni'],
-						'tipo_documento_identidad' if party_type == 'Customer' else 'nombre_tipo_documento': 'DOCUMENTO NACIONAL DE IDENTIDAD (DNI)',
-						'codigo_tipo_documento': '1',
-						'party_type': 'Individual'
-					})
-					party.save()
-				return party.name
+					content = {
+						'token': token,
+						'dni': tax_id
+					}
+				response = requests.post(url, headers=headers, data=json.dumps(content))
+
+				party_info = json.loads(response.content)
+				if party_info['success']:
+					if len(tax_id) == 11:
+						party = frappe.get_doc({
+							'doctype': party_type,
+							'party_name': party_info['nombre_o_razon_social'],
+							party_type.lower() + '_name': party_info['nombre_o_razon_social'],
+							'tax_id': party_info['ruc'],
+							'tipo_documento_identidad' if party_type == 'Customer' else 'nombre_tipo_documento': 'REGISTRO ÚNICO DE CONTRIBUYENTES',
+							'codigo_tipo_documento': '6'
+						})
+						party.save()
+
+						address_shipping = frappe.get_doc({
+							'doctype': 'Address',
+							'address_title': party_info['nombre_o_razon_social'],
+							'address_type': 'Shipping',
+							'address_line1': party_info['direccion_completa'],
+							'distrito': party_info['distrito'],
+							'provincia': party_info['provincia'],
+							'departamento': party_info['departamento'],
+							'country': 'Peru'
+						})
+						address_shipping.save()
+
+						address_billing = frappe.get_doc({
+							'doctype': 'Address',
+							'address_title': party_info['nombre_o_razon_social'],
+							'address_type': 'Billing',
+							'address_line1': party_info['direccion_completa'],
+							'distrito': party_info['distrito'],
+							'provincia': party_info['provincia'],
+							'departamento': party_info['departamento'],
+							'country': 'Peru'
+						})
+						address_billing.save()
+
+						shipping_dynamic_link = frappe.get_doc({
+									'doctype': 'Dynamic Link',
+									'link_doctype': party_type,
+									'link_name': party.name,
+									'parenttype': 'Address',
+									'parent': address_shipping.name
+								})
+						shipping_dynamic_link.save()
+
+						billing_dynamic_link = frappe.get_doc({
+									'doctype': 'Dynamic Link',
+									'link_doctype': party_type,
+									'link_name': party.name,
+									'parenttype': 'Address',
+									'parent': address_billing.name
+								})
+						billing_dynamic_link.save()
+											
+					elif len(tax_id) == 8:
+						party = frappe.get_doc({
+							'doctype': party_type,
+							'party_name': party_info['nombre_completo'],
+							party_type.lower() + '_name': party_info['nombre_completo'],
+							'tax_id': party_info['dni'],
+							'tipo_documento_identidad' if party_type == 'Customer' else 'nombre_tipo_documento': 'DOCUMENTO NACIONAL DE IDENTIDAD (DNI)',
+							'codigo_tipo_documento': '1',
+							'party_type': 'Individual'
+						})
+						party.save()
+					return party.name
+				else:
+					return None
 			else:
 				return None
 		else:
